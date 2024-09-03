30408d423e3d05fe5d6b7215e298f8a5614ae137
Alondra Franco <alondralezfra@hotmail.com>
2024-09-03 12:27:55 -0500

    Added Text to Goals Tab

--- Active/app/(tabs)/goals.tsx
+++ Active/app/(tabs)/goals.tsx
@@ -5,7 +5,9 @@ const goals = () => {
   return (
     <>
       <View style={styles.container}>
-        <Text>goals</Text>
+        <Text style={styles.title}>Your Monthly Goals</Text>
+        <Text style={styles.subtitle}>September</Text>
+        <Text style={styles.list}> Your Goals List </Text>
       </View>
       
     </>
@@ -17,7 +19,44 @@ export default goals
 const styles = StyleSheet.create({
   container:{
     flex: 1, 
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
     alignItems: 'center'
+  },
+  title: {
+    marginTop: 20,
+    paddingVertical: 7,
+    paddingHorizontal: 7,
+    borderWidth: 3,
+    borderColor: '#20232a',
+    borderRadius: 7,
+    backgroundColor: '#eaeacf',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 25,
+    fontWeight: '500'
+  },
+  subtitle: {
+    paddingVertical: 55,
+    paddingHorizontal: 35,
+    borderWidth: 1,
+    borderColor: '#20232a',
+    borderRadius: 7,
+    backgroundColor: '#d9e8ed',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 25,
+    fontWeight: '500'
+  },
+  list: {
+    paddingVertical: 7,
+    paddingHorizontal: 7,
+    borderWidth: 1,
+    borderColor: '#20232a',
+    borderRadius: 7,
+    backgroundColor: '#ebdfec',
+    color: '#20232a',
+    textAlign: 'center',
+    fontSize: 25,
+    fontWeight: '500'
   }
 })
