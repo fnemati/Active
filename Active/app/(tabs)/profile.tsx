52f0297b4dc723ad571027b0854318620e03cb77
Numbatt <diego.rico10010@gmail.com>
2024-09-07 19:40:35 -0500

    Build profile page and added profile picture

--- Active/app/(tabs)/profile.tsx
+++ Active/app/(tabs)/profile.tsx
@@ -1,20 +1,134 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView } from "react-native";
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 
 const profile = () => {
   return (
-    <View style={styles.container}>
-      <Text>Profile</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.titleBar}>
+          <MaterialIcons name="arrow-back-ios" size={24} color="5257D"></MaterialIcons>
+          <MaterialIcons name="more-vert" size={24} color="5257D"></MaterialIcons>
+        </View>
+
+        <View style={{alignSelf: "center"}}>
+          <View style={styles.profileImage}>
+            <Image source={require('../../assets/images/profile-pic.jpg')} style={styles.image} resizeMode="center"></Image>
+          </View>
+          <View style={styles.dm}>
+            <MaterialIcons name="chat" size={18} color="#DFD8C8"></MaterialIcons>
+          </View>
+          <View style={styles.active}></View>
+          <View style={styles.add}>
+            <Ionicons name="add" size={48} color="DFD8C8" style={{ marginTop: 6, marginLeft: 2 }}></Ionicons>
+          </View>
+        </View>
+
+        <View style={styles.infoContainer}>
+          <Text style={[styles.text, { fontWeight: "200", fontSize: 36 }]}>Diego Rico</Text>
+          <Text style={[styles.text, { color: "AEB5BC", fontSize: 14 , marginTop: 24}]}>Personal Records</Text>
+        </View>
+
+        <View style={styles.statsContainer}>
+          <View style={styles.statsBox}>
+            <Text style={[styles.text, { fontSize: 24 }]}>174</Text>
+            <Text style={[styles.text, styles.subText]}>All-time Workouts</Text>
+          </View>
+          <View style={[styles.statsBox, {borderColor: "#DFD8C8", borderLeftWidth: 1, borderRightWidth: 1, width: 100}]}>
+            <Text style={[styles.text, { fontSize: 24 }]}>73.6K</Text>
+            <Text style={[styles.text, styles.subText]}>Calories Burned</Text>
+          </View>
+          <View style={styles.statsBox}>
+            <Text style={[styles.text, { fontSize: 24 }]}>525 lbs</Text>
+            <Text style={[styles.text, styles.subText]}>Big 3 Total</Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default profile
 
 const styles = StyleSheet.create({
-  container:{
-    flex: 1, 
-    justifyContent: 'center',
-    alignItems: 'center'
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    fontFamily: "HelveticaNeue",
+    color: "#52575D",
+
+  },
+  subText: {
+    fontSize: 12,
+    color: "#AEB5BC",
+    textTransform: "uppercase",
+    fontWeight: "500",
+  },
+  image: {
+    flex: 1,
+    width: undefined,
+    height: undefined,
+
+  },
+  titleBar: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 24,
+    marginHorizontal: 16,
+  },
+  profileImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 150,
+    overflow: "hidden"
+  },
+  dm: {
+    backgroundColor: "#41444B",
+    position: "absolute",
+    top: 20,
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  active: {
+    backgroundColor: "34FFB9",
+    position: "absolute",
+    bottom: 28,
+    left: 10,
+    padding: 4,
+    height: 20,
+    width: 20,
+    borderRadius: 10
+  },
+  add: {
+    backgroundColor: "#41444B",
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    width: 0,
+    height: 0,
+    borderRadius: 30,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  infoContainer: {
+    alignSelf: "center",
+    alignItems: "center",
+    marginTop: 16
+  },
+  statsContainer: {
+    flexDirection: "row",
+    alignSelf: "center",
+    marginTop: 8,
+  },
+  statsBox: {
+    alignItems: "center",
+    flex: 2,
   }
 })
--- Active/app/index.tsx
+++ Active/app/index.tsx
@@ -1,19 +1,40 @@
 import { Link } from "@react-navigation/native";  
 import * as React from "react";
 import { Text, View, StyleSheet } from "react-native";
-import Icon from 'react-native-vector-icons/FontAwesome';
 
-const ProfileSettingsText = () => {
-  return <Text style={styles.profileSettings}>Profile Settings</Text>;
-};
 
-const EditInfoText = () => {
-  return <Text style={styles.editInfo}>Edit Info</Text>;
-};
 
-const Rectangle = () => {
-  return <View style={styles.rectangleView} />;
-};
+export default function Index() {
+  console.log("App executed!");
+  return (
+    <View style={styles.container}>
+      <Link to="/home" style={{ color: 'blue' }}>Go to home</Link>
+      {/* <Rectangle /> */}
+      {/* <Profile /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+// ALL UNUSED CODE FOR PROFILE BELOW (tbh we can delete it)
+// 
+// const ProfileSettingsText = () => {
+//   return <Text style={styles.profileSettings}>Profile Settings</Text>;
+// };
+
+// const EditInfoText = () => {
+//   return <Text style={styles.editInfo}>Edit Info</Text>;
+// };
+
+// const Rectangle = () => {
+//   return <View style={styles.rectangleView} />;
+// };
 
 // const Profile = () => {
 //   return (
@@ -41,205 +62,187 @@ const Rectangle = () => {
 //   );
 // };
 
-export default function Index() {
-  console.log("App executed!");
-  return (
-    <View style={styles.container}>
-      <Link to="/home" style={{ color: 'blue' }}>Go to home</Link>
-      {/* <Rectangle /> */}
-      {/* <Profile /> */}
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  profileSettings: {
-    fontSize: 32,
-    fontWeight: "500",
-    fontFamily: "Arial",
-    color: "#000",
-    textAlign: "center",
-    width: 220,
-    height: 48,
-    position: "absolute",
-    top: 150,
-    left: 112,
-    zIndex: 10, // Ensure text is on top
-  },
-  editInfo: {
-    fontSize: 27,
-    fontWeight: "500",
-    fontFamily: "Arial",
-    color: "#000",
-    textAlign: "center",
-    width: 102,
-    height: 40,
-    position: "absolute",
-    top: 205,
-    left: 112,
-    zIndex: 10, // Ensure text is on top
-  },
-  profileLayout: {
-    height: 2,
-    width: 393,
-  },
-  profileTypo: {
-    height: 48,
-    fontSize: 32,
-    textAlign: "center",
-    color: "#000",
-    fontFamily: "Arial",
-    fontWeight: "500",
-  },
-  iconPosition: {
-    left: 12,
-    position: "absolute",
-  },
-  lineIconPosition: {
-    left: 1,
-    height: 2,
-    width: 393,
-    position: "absolute",
-  },
-  iconLayout1: {
-    height: 60,
-    width: 60,
-  },
-  iconLayout: {
-    height: 61,
-    position: "absolute",
-  },
-  profileIcon: {
-    top: 143,
-    left: 31,
-    fontSize: 62,
-    color: "#000", // Ensure the icon is visible
-    position: "absolute",
-  },
-  profileChild: {
-    top: 637,
-    left: 0,
-    height: 2,
-    position: "absolute",
-    backgroundColor: "#ccc",
-  },
-  personalRecords: {
-    top: 555,
-    width: 251,
-    left: 115,
-    fontSize: 32,
-    position: "absolute",
-    zIndex: 10, // Ensure text is on top
-  },
-  profileItem: {
-    top: 519,
-    left: 0,
-    height: 2,
-    position: "absolute",
-    backgroundColor: "#ccc",
-  },
-  profileInner: {
-    top: 283,
-    height: 2,
-    width: 393,
-    backgroundColor: "#ccc",
-  },
-  lineIcon: {
-    top: 132,
-    backgroundColor: "#ccc",
-  },
-  profileChild1: {
-    top: 742,
-    backgroundColor: "#ccc",
-  },
-  profileChild2: {
-    top: 402,
-    left: -12,
-    maxHeight: "100%",
-    width: 417,
-    position: "absolute",
-    backgroundColor: "#ccc",
-  },
-  editAvatar: {
-    top: 312,
-    width: 161,
-    left: 115,
-    fontSize: 32,
-    position: "absolute",
-    zIndex: 10, // Ensure text is on top
-  },
-  workoutHeatmap: {
-    top: 437,
-    width: 261,
-    left: 115,
-    fontSize: 32,
-    position: "absolute",
-    zIndex: 10, // Ensure text is on top
-  },
-  icon: {
-    top: 765,
-    left: 12,
-    fontSize: 60,
-    color: "#000", // Ensure the icon is visible
-    position: "absolute",
-  },
-  calendarIcon: {
-    top: 763,
-    left: 205,
-    fontSize: 65,
-    color: "#000", // Ensure the icon is visible
-    position: "absolute",
-  },
-  icon2: {
-    left: 304,
-    width: 61,
-    top: 764,
-    fontSize: 61,
-    color: "#000", // Ensure the icon is visible
-    position: "absolute",
-  },
-  groupIcon: {
-    top: 13,
-    width: 52,
-    height: 53,
-    fontSize: 53,
-    color: "#000", // Ensure the icon is visible
-    position: "absolute",
-  },
-  profile1: {
-    top: 75,
-    left: 144,
-    width: 94,
-    position: "absolute",
-    zIndex: 10, // Ensure text is on top
-  },
-  gearIcon: {
-    top: 12,
-    left: 313,
-    width: 63,
-    height: 63,
-    fontSize: 63,
-    color: "#000", // Ensure the icon is visible
-    position: "absolute",
-  },
-  rectangleView: {
-    backgroundColor: "#e7e4e4",
-    height: 507,
-    top: 132,
-    width: 393,
-    left: 0,
-    position: "absolute",
-  },
-  profile: {
-    backgroundColor: "#f5eeee",
-    flex: 1,
-    width: "100%",
-    height: 852,
-    overflow: "hidden",
-  },
-});
+  // profileSettings: {
+  //   fontSize: 32,
+  //   fontWeight: "500",
+  //   fontFamily: "Arial",
+  //   color: "#000",
+  //   textAlign: "center",
+  //   width: 220,
+  //   height: 48,
+  //   position: "absolute",
+  //   top: 150,
+  //   left: 112,
+  //   zIndex: 10, // Ensure text is on top
+  // },
+  // editInfo: {
+  //   fontSize: 27,
+  //   fontWeight: "500",
+  //   fontFamily: "Arial",
+  //   color: "#000",
+  //   textAlign: "center",
+  //   width: 102,
+  //   height: 40,
+  //   position: "absolute",
+  //   top: 205,
+  //   left: 112,
+  //   zIndex: 10, // Ensure text is on top
+  // },
+  // profileLayout: {
+  //   height: 2,
+  //   width: 393,
+  // },
+  // profileTypo: {
+  //   height: 48,
+  //   fontSize: 32,
+  //   textAlign: "center",
+  //   color: "#000",
+  //   fontFamily: "Arial",
+  //   fontWeight: "500",
+  // },
+  // iconPosition: {
+  //   left: 12,
+  //   position: "absolute",
+  // },
+  // lineIconPosition: {
+  //   left: 1,
+  //   height: 2,
+  //   width: 393,
+  //   position: "absolute",
+  // },
+  // iconLayout1: {
+  //   height: 60,
+  //   width: 60,
+  // },
+  // iconLayout: {
+  //   height: 61,
+  //   position: "absolute",
+  // },
+  // profileIcon: {
+  //   top: 143,
+  //   left: 31,
+  //   fontSize: 62,
+  //   color: "#000", // Ensure the icon is visible
+  //   position: "absolute",
+  // },
+  // profileChild: {
+  //   top: 637,
+  //   left: 0,
+  //   height: 2,
+  //   position: "absolute",
+  //   backgroundColor: "#ccc",
+  // },
+  // personalRecords: {
+  //   top: 555,
+  //   width: 251,
+  //   left: 115,
+  //   fontSize: 32,
+  //   position: "absolute",
+  //   zIndex: 10, // Ensure text is on top
+  // },
+  // profileItem: {
+  //   top: 519,
+  //   left: 0,
+  //   height: 2,
+  //   position: "absolute",
+  //   backgroundColor: "#ccc",
+  // },
+  // profileInner: {
+  //   top: 283,
+  //   height: 2,
+  //   width: 393,
+  //   backgroundColor: "#ccc",
+  // },
+  // lineIcon: {
+  //   top: 132,
+  //   backgroundColor: "#ccc",
+  // },
+  // profileChild1: {
+  //   top: 742,
+  //   backgroundColor: "#ccc",
+  // },
+  // profileChild2: {
+  //   top: 402,
+  //   left: -12,
+  //   maxHeight: "100%",
+  //   width: 417,
+  //   position: "absolute",
+  //   backgroundColor: "#ccc",
+  // },
+  // editAvatar: {
+  //   top: 312,
+  //   width: 161,
+  //   left: 115,
+  //   fontSize: 32,
+  //   position: "absolute",
+  //   zIndex: 10, // Ensure text is on top
+  // },
+  // workoutHeatmap: {
+  //   top: 437,
+  //   width: 261,
+  //   left: 115,
+  //   fontSize: 32,
+  //   position: "absolute",
+  //   zIndex: 10, // Ensure text is on top
+  // },
+  // icon: {
+  //   top: 765,
+  //   left: 12,
+  //   fontSize: 60,
+  //   color: "#000", // Ensure the icon is visible
+  //   position: "absolute",
+  // },
+  // calendarIcon: {
+  //   top: 763,
+  //   left: 205,
+  //   fontSize: 65,
+  //   color: "#000", // Ensure the icon is visible
+  //   position: "absolute",
+  // },
+  // icon2: {
+  //   left: 304,
+  //   width: 61,
+  //   top: 764,
+  //   fontSize: 61,
+  //   color: "#000", // Ensure the icon is visible
+  //   position: "absolute",
+  // },
+  // groupIcon: {
+  //   top: 13,
+  //   width: 52,
+  //   height: 53,
+  //   fontSize: 53,
+  //   color: "#000", // Ensure the icon is visible
+  //   position: "absolute",
+  // },
+  // profile1: {
+  //   top: 75,
+  //   left: 144,
+  //   width: 94,
+  //   position: "absolute",
+  //   zIndex: 10, // Ensure text is on top
+  // },
+  // gearIcon: {
+  //   top: 12,
+  //   left: 313,
+  //   width: 63,
+  //   height: 63,
+  //   fontSize: 63,
+  //   color: "#000", // Ensure the icon is visible
+  //   position: "absolute",
+  // },
+  // rectangleView: {
+  //   backgroundColor: "#e7e4e4",
+  //   height: 507,
+  //   top: 132,
+  //   width: 393,
+  //   left: 0,
+  //   position: "absolute",
+  // },
+  // profile: {
+  //   backgroundColor: "#f5eeee",
+  //   flex: 1,
+  //   width: "100%",
+  //   height: 852,
+  //   overflow: "hidden",
+  // },
